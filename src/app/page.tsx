a61cf51dae6213143c252491308dd30775727bd3
Alex Naltu <naltuok@gmail.com>
2024-07-03 08:07:03 +0200

    added categories grid

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,6 +1,9 @@
 import HeroCarousel from "@/components/carousel/hero-carousel";
 import TopCarousel from "@/components/carousel/top-carousel";
 import Navbar from "@/components/navbar/navbar";
+import { categoryItems } from "@/lib/constants";
+import Image from "next/image";
+import Link from "next/link";
 
 export default function Home() {
   return (
@@ -11,6 +14,21 @@ export default function Home() {
       <h1 className="font-black uppercase text-xs tracking-tighter bg-primary py-2 px-1 text-white w-fit">
         Free shipping for orders over 80$
       </h1>
+      <div className="grid grid-cols-2 gap-2 my-3">
+        {categoryItems.map((category) => (
+          <Link href={category.href} key={category.href}>
+            <Image
+              src={category.image}
+              alt={category.href}
+              width={300}
+              height={300}
+            />
+          </Link>
+        ))}
+      </div>
+      <h2 className="uppercase mb-4 font-bold tracking-tighter">
+        Featured Products
+      </h2>
     </div>
   );
 }
--- src/lib/constants.ts
+++ src/lib/constants.ts
@@ -1,11 +1,30 @@
 export const heroCarouselImages = [
   {
-    image: "/carousel/hero1.svg",
+    image: "/carousel/hero1.png",
   },
   {
-    image: "/carousel/hero2.svg",
+    image: "/carousel/hero2.png",
   },
   {
-    image: "/carousel/hero3.svg",
+    image: "/carousel/hero3.png",
+  },
+];
+
+export const categoryItems = [
+  {
+    href: "/category/t-shirts",
+    image: "/categories/tshirts.png",
+  },
+  {
+    href: "/category/best-sellers",
+    image: "/categories/bestsellers.png",
+  },
+  {
+    href: "/category/sweatshirt.png",
+    image: "/categories/sweatshirt.png",
+  },
+  {
+    href: "/category/hoodies",
+    image: "/categories/hoodies.png",
   },
 ];
