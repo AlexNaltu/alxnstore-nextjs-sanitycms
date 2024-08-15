277ba243150d2971aed104f71cc5923ba838e00b
Alex Naltu <naltuok@gmail.com>
2024-08-15 12:43:10 +0200

    added faq to the contact and cart page

---
+++ src/components/accordions/questions-accordion.tsx
@@ -0,0 +1,172 @@
+import React from "react";
+
+import {
+  Accordion,
+  AccordionItem,
+  AccordionTrigger,
+  AccordionContent,
+} from "../ui/accordion";
+
+const QuestionsAccordion = () => {
+  return (
+    <div>
+      <Accordion type="single" collapsible>
+        <AccordionItem value="item-1" className="">
+          <AccordionTrigger className="decoration-0 text-base min-[470px]:text-lg sm:text-xl md:text-2xl">
+            Shipping
+          </AccordionTrigger>
+          <AccordionContent>
+            <section className="flex flex-col gap-2">
+              <div className="flex flex-col gap-2">
+                <h1 className="text-black text-lg">
+                  Do you ship internationally?
+                </h1>
+                <p className="text-slate-500">
+                  We ship to all European countries. The shipping cost is based
+                  on the first item plus any additional items and can be viewed
+                  in your cart or on the checkout page.
+                </p>
+              </div>
+              <div className="flex flex-col gap-2">
+                <h1 className="text-black text-lg">
+                  When will my order arrive?
+                </h1>
+                <p className="text-slate-500">
+                  Delivery times depend on your location and the shipping method
+                  chosen. Standard shipping usually takes 3-10 business days.
+                </p>
+              </div>
+              <div className="flex flex-col gap-2">
+                <h1 className="text-black text-lg">
+                  How much does shipping cost?
+                </h1>
+                <p className="text-slate-500">
+                  Shipping cost is calculated on the cart page or during
+                  checkout, but usually, it costs 5€ for the first item and 1€
+                  for each additional item
+                </p>
+              </div>
+            </section>
+          </AccordionContent>
+        </AccordionItem>
+        <AccordionItem value="item-2" className="">
+          <AccordionTrigger className="decoration-0 text-base min-[470px]:text-lg sm:text-xl md:text-2xl">
+            Payment
+          </AccordionTrigger>
+          <AccordionContent>
+            <section className="flex flex-col gap-2">
+              <div className="flex flex-col gap-2">
+                <h1 className="text-black text-lg">
+                  What payment methods do you accept?
+                </h1>
+                <p className="text-slate-500">
+                  We accept major credit cards &#40; Visa, MasterCard &#41; ,
+                  PayPal, and Revolut Pay as payment methods.
+                </p>
+              </div>
+              <div className="flex flex-col gap-2">
+                <h1 className="text-black text-lg">
+                  Is it safe to use my credit card on your website?
+                </h1>
+                <p className="text-slate-500">
+                  We use Stripe for payment processing, which is highly secure.
+                </p>
+              </div>
+              <div className="flex flex-col gap-2">
+                <h1 className="text-black text-lg">
+                  What should I do if my payment is declined?
+                </h1>
+                <p className="text-slate-500">
+                  Please check with your bank or credit card provider for any
+                  issues with your card. You can also try using a different
+                  payment method.
+                </p>
+              </div>
+            </section>
+          </AccordionContent>
+        </AccordionItem>
+        <AccordionItem value="item-3" className="">
+          <AccordionTrigger className="decoration-0 text-base min-[470px]:text-lg sm:text-xl md:text-2xl">
+            Returns and Exchanges
+          </AccordionTrigger>
+          <AccordionContent>
+            <section className="flex flex-col gap-2">
+              <div className="flex flex-col gap-2">
+                <h1 className="text-black text-lg">
+                  What is your return policy?
+                </h1>
+                <p className="text-slate-500">
+                  We accept returns within 14 days of purchase. Items must be in
+                  their original condition and packaging.
+                </p>
+              </div>
+              <div className="flex flex-col gap-2">
+                <h1 className="text-black text-lg">How do I return an item?</h1>
+                <p className="text-slate-500">
+                  To return an item, please contact our customer service team
+                  for instructions.
+                </p>
+              </div>
+              <div className="flex flex-col gap-2">
+                <h1 className="text-black text-lg">
+                  Are return shipping costs covered?
+                </h1>
+                <p className="text-slate-500">
+                  Return shipping costs are the responsibility of the customer,
+                  unless the return is due to a mistake on our part.
+                </p>
+              </div>
+              <div className="flex flex-col gap-2">
+                <h1 className="text-black text-lg">Can I exchange an item?</h1>
+                <p className="text-slate-500">
+                  No, exchanges are not accepted. We only accept returns if the
+                  item is in its original condition. Please contact us to
+                  arrange a return.
+                </p>
+              </div>
+            </section>
+          </AccordionContent>
+        </AccordionItem>
+        <AccordionItem value="item-4" className="">
+          <AccordionTrigger className="decoration-0 text-base min-[470px]:text-lg sm:text-xl md:text-2xl">
+            Account
+          </AccordionTrigger>
+          <AccordionContent>
+            <section className="flex flex-col gap-2">
+              <div className="flex flex-col gap-2">
+                <h1 className="text-black text-lg">
+                  Do I need an account to place an order?
+                </h1>
+                <p className="text-slate-500">
+                  Yes, creating an account is required to make a purchase. It
+                  ensures that we can quickly assist you with any issues and
+                  makes future purchases faster and easier.
+                </p>
+              </div>
+              <div className="flex flex-col gap-2">
+                <h1 className="text-black text-lg">
+                  How do I reset my password?
+                </h1>
+                <p className="text-slate-500">
+                  Click on the “Forgot Password” link on the login page and
+                  follow the instructions to reset your password.
+                </p>
+              </div>
+              <div className="flex flex-col gap-2">
+                <h1 className="text-black text-lg">
+                  How can I contact customer service?
+                </h1>
+                <p className="text-slate-500">
+                  You can reach our customer service team via email or phone.
+                  For further details, please visit our Contact Us page.
+                </p>
+              </div>
+            </section>
+          </AccordionContent>
+        </AccordionItem>
+      </Accordion>
+    </div>
+  );
+};
+
+export default QuestionsAccordion;
