d85acf67db1418fd6aad87811e17753be1f2d00e
Alex Naltu <naltuok@gmail.com>
2024-08-16 14:31:39 +0200

    added terms of use and privacy policy pages

---
+++ src/app/(routes)/(home)/(legal-information)/privacy-policy/page.tsx
@@ -0,0 +1,31 @@
+import { privacyPolicy, termsOfUse } from "@/lib/constants";
+import React from "react";
+
+const TermsOfUse = () => {
+  return (
+    <div className="max-w-[1000px] mx-auto text-white flex flex-col gap-2 my-3 sm:my-5 md:my-7  px-2">
+      <h1 className="text-3xl underline mb-4">Terms Of Use</h1>
+      {privacyPolicy.map((item, i) => (
+        <div key={i} className="flex flex-col gap-2">
+          <h1 className="text-xl font-bold">{item.title}</h1>
+          <div>
+            <p className="text-sm">{item.item1}</p>
+            <p className="text-sm">{item.item2}</p>
+            <p className="text-sm">{item.item2}</p>
+          </div>
+        </div>
+      ))}
+
+      <ul className="mt-5 text- text-slate-200 list-disc">
+        <li>Company Name: AlxnBusiness</li>
+        <li>Email: alxnbusiness1@gmail.com</li>
+        <li>Phone: +49 1515 7491907</li>
+        <li>
+          Address: Erdmarsgasse 4, Kaltenlengsfeld, Meiningen, 36452, Germany
+        </li>
+      </ul>
+    </div>
+  );
+};
+
+export default TermsOfUse;
--- src/lib/constants.tsx
+++ src/lib/constants.tsx
@@ -1,3 +1,4 @@
+import { title } from "process";
 import { AiFillFacebook } from "react-icons/ai";
 import { FaInstagramSquare, FaYoutube, FaLinkedin } from "react-icons/fa";
 import { IoLogoTiktok } from "react-icons/io5";
@@ -97,11 +98,11 @@ export const menuNavLinks = [
 
 export const legalDocs = [
   {
-    href: "/privacy",
+    href: "/terms-of-use",
     title: "Terms of Use",
   },
   {
-    href: "/terms",
+    href: "/privacy-policy",
     title: "Privacy Policy",
   },
   {
@@ -263,5 +264,197 @@ export const payments = [
   },
   {
     image: "/payments/klarna.png",
+  },
+];
+
+export const termsOfUse = [
+  {
+    title: "Platform Overview",
+    item1:
+      "Our Website offers a variety of clothing products, including t-shirts and hoodies, for personal use. We are committed to providing a safe and enjoyable shopping experience for users located within the European Union, the United Kingdom, Iceland, Liechtenstein, and Norway.",
+    item2: "",
+  },
+  {
+    title: "User Eligibility",
+    item1:
+      "Age Requirement: You must be at least 18 years old to use our Website. Users under 13 are not permitted to create an account or make purchases on the platform.",
+    item2:
+      "Account Creation: Users may create an account to facilitate faster checkout and manage their orders. However, linking your account to social media platforms is not supported.",
+  },
+  {
+    title: "Purchases and Payments",
+    item1:
+      "Accepted Payment Methods: We accept Visa, Mastercard, Revolut, PayPal, and Klarna. All payments must be made in Euros (€).",
+    item2:
+      "Refunds and Returns: You may return purchased items according to our Return Policy, which is posted on the Website. Refunds will be processed according to the original payment method.",
+  },
+  {
+    title: "Prohibited Activities",
+    item1:
+      "By using our Website, you agree not to engage in any of the following prohibited activities: ",
+
+    item2:
+      "Use bots or automated methods to interact with the platform / Advertise or offer to sell goods and services not authorized by us / Sell or otherwise transfer your user account",
+  },
+  {
+    title: "Platform Content",
+    item1:
+      "All content provided on the Website, including product descriptions, images, and designs, is owned by AlxnBusiness. Users are prohibited from uploading or posting their own content, such as comments, reviews, or media files.",
+    item2: "",
+  },
+  {
+    title: "Third-Party Websites",
+    item1:
+      "Our Website may contain links to social media accounts and other third-party websites that are not owned or controlled by AlxnBusiness. We are not responsible for the content, policies, or practices of these external sites.",
+    item2: "",
+  },
+  {
+    title: "Dispute Resolution",
+    item1:
+      "In the event of any disputes between you and AlxnBusiness, we will first attempt to resolve the issue through informal negotiations. If the dispute is not resolved within 30 days, it will be submitted to arbitration in Frankfurt, Germany, with proceedings conducted in English.",
+    item2: "",
+  },
+  {
+    title: "Limitation of Liability",
+    item1:
+      "Our liability is limited to the amount you paid for the goods in question. We do not offer subscription services, so this limitation does not extend beyond individual transactions.",
+    item2: "",
+  },
+  {
+    title: "Governing Law",
+    item1:
+      "These Terms are governed by and interpreted under the laws of Germany, specifically the laws of Frankfurt.",
+    item2: "",
+  },
+];
+
+export const privacyPolicy = [
+  {
+    title: "Introduction",
+    item1:
+      "Welcome to AlxnStore! This Privacy Policy describes how AlxnBusiness collects, uses, and discloses your personal information when you visit or make a purchase from www.alxnstore.com .",
+    item2: "",
+  },
+  {
+    title: "Personal Information We Collect",
+    item1:
+      "When you visit the Site, we collect certain information about your device, your interaction with the Site, and information necessary to process your purchases. We may also collect additional information if you contact us for customer support.",
+    item2:
+      "Personal Information Provided Directly by You: Names, email addresses, and phone numbers, usernames, Contact preferences Billing addresses/ Debit/credit card numbers via Stripe",
+    item3:
+      "Social Media Information: If you choose to register using a social media account (e.g., Facebook or Google), we may collect information from those accounts.",
+  },
+  {
+    title: "How We Use Your Personal Information",
+    item1:
+      "We use the information we collect to operate, improve, and provide our services, including:",
+    item2:
+      "To provide and manage our services: We use your personal information to fulfill orders, provide customer support, and maintain your account.To prevent fraudulent activities: We monitor for and protect against fraudulent transactions and other illegal activities.For compliance with legal obligations: We may process your data to comply with applicable laws and regulations.",
+  },
+  {
+    title: "Sharing Your Personal Information",
+    item1:
+      "We do not share your personal information with third parties except as necessary to operate our services, including:",
+    item2:
+      "Payment Processors: We use Stripe to process payments. Stripe's Privacy Policy governs their use of your payment information. Legal Requirements: We may disclose your information to comply with legal obligations or to protect our rights.",
+  },
+  {
+    title: "Security of Your Personal Information",
+    item1:
+      "We take reasonable steps to protect your personal information from unauthorized access, use, or disclosure. However, no internet or email transmission is ever fully secure or error-free.",
+    item2: "",
+  },
+  {
+    title: "Data Retention",
+    item1:
+      "We retain your personal information for as long as you have an account with us. If you delete your account, we may retain certain information as required by law or for legitimate business purposes.",
+    item2: "",
+  },
+  {
+    title: "User Rights",
+    item1:
+      "Users can update or delete their account information by logging into their account settings. You may also contact us at alxnbusiness1@gmail.com for further assistance.",
+    item2: "",
+  },
+  {
+    title: "Cookies and Tracking Technologies",
+    item1:
+      "We use cookies and local storage to improve your experience on the Site. For more details, please visit our Cookie Policy.",
+    item2: "",
+  },
+  {
+    title: "International Transfers",
+    item1: "We do not transfer your data outside the EU or UK.",
+    item2: "",
+  },
+  {
+    title: " Your Consent",
+    item1:
+      "By using the Site, you consent to the processing of your personal information as described in this Privacy Policy.",
+    item2: "",
+  },
+  {
+    title: "Changes to This Policy",
+    item1:
+      "We may update this Privacy Policy from time to time. If we make significant changes, we will notify you via email or through a notice on our website.",
+    item2: "",
+  },
+];
+
+export const refundPolicy = [
+  {
+    title: "Return Policy Overview",
+    item1:
+      "At AlxnStore, we strive to ensure that our customers are fully satisfied with their purchases. If for any reason you are not satisfied, we offer the following return policy.",
+    item2: "",
+    item3: "",
+  },
+  {
+    title: "Return Eligibility",
+    item1:
+      "Refunds Only: Customers are eligible to receive a refund for returned items.",
+    item2:
+      "Non-Discounted Items: Only non-discounted items are eligible for return. We do not offer discounts on our products.",
+    item3:
+      "Excluded Items: All items purchased from our website are eligible for return, with no exclusions.",
+  },
+  {
+    title: "Return Process",
+    item1:
+      "Authorization Required: Customers must contact us before returning any item to receive return authorization.",
+    item2:
+      "What to Include: When returning an item, customers must include the item itself, proof of purchase, and the information used to purchase the product.",
+    item3:
+      "Return Timeframe: Customers must postmark their return packages within 14 days of the purchase date.",
+    item4:
+      "Processing Time: We will process returns within 5 days of receiving the returned item. Customers will receive an email notification once their return has been processed.",
+  },
+  {
+    title: "Shipping Fees",
+    item1:
+      "Customer Pays: Customers are responsible for paying the return shipping fees. We do not include prepaid shipping labels.",
+    item2: "",
+    item3: "",
+  },
+
+  {
+    title: "Restocking Fees",
+    item1: "We do not charge any restocking fees for returned items.",
+    item2: "",
+    item3: "",
+  },
+  {
+    title: "Additional Guidelines",
+    item1:
+      "Return Timeframe: Items must be returned within 14 days from the date of purchase to be eligible for a refund.",
+    item2: "",
+    item3: "",
+  },
+  {
+    title: "Customer Service",
+    item1:
+      "If you have any questions or need further assistance, please contact our customer service team:",
+    item2: "Phone Number: +49 1515 7491907",
+    item3: "Email: alxnbusiness1@gmail.com",
   },
 ];
