0ce0fb9dfe923ec76aed6d17ea287784aaeec978
Alex Naltu <naltuok@gmail.com>
2024-08-25 09:39:46 +0200

    sign in / sign up metadata

--- src/app/(routes)/(auth)/sign-up/[[...sign-up]]/page.tsx
+++ src/app/(routes)/(auth)/sign-up/[[...sign-up]]/page.tsx
@@ -1,5 +1,19 @@
 import { SignUp } from "@clerk/nextjs";
+import { Metadata } from "next";
 import React from "react";
+
+export const metadata: Metadata = {
+  metadataBase: new URL("https://alxnstore.shop"),
+  keywords: ["E-commerce", "Clothing", "Fashion", "Store"],
+  title: {
+    default: "Sign Up",
+    template: "%s | Alxn Store",
+  },
+  openGraph: {
+    description: "Alxn Store is a clothing store",
+    images: [],
+  },
+};
 
 const SignUpPage = () => {
   return (
